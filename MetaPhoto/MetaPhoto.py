a639fd962ee0c9a0bf014d321374bf4e5454b0c6
Mario Vormstein <github@hquer.eu>
2020-01-19 21:01:51 +0100

    Add a method which moves/renames a given MetaPicture to a new directory

--- MetaPhoto/MetaPhoto.py
+++ MetaPhoto/MetaPhoto.py
@@ -1,5 +1,7 @@
 from os import listdir
 from os.path import isfile, join
+from pathlib import Path
+from shutil import move
 
 from exif import Image
 
@@ -9,6 +11,7 @@ class MetaPhoto:
     Copy images to a folder structure based on the creation date.
     Name the files based on date, a tag, and the original name.
     """
+
     def __init__(self, input_folder):
         self.input_folder = input_folder
         self.raw_pictures = []
@@ -24,11 +27,22 @@ class MetaPhoto:
         """ Convert the found files to objects handling exif information """
         self.meta_pictures = [MetaPicture(image) for image in self.raw_pictures]
 
+    @staticmethod
+    def _move_picture(picture: 'MetaPicture', target_directory: Path):
+        """ Move a given MetaPicture to a new directory. Rename it based on it's date """
+        file_name = picture.picture_path.name
+        # todo Use a method which handles the date format and is not private
+        date = picture._get_date()
+        new_file_name = f"{date}_{file_name}"
+        target_path = join(target_directory, new_file_name)
+        move(picture.picture_path, target_path)
+
 
 class MetaPicture:
     """ Proxy for the exif.Image class"""
+
     def __init__(self, picture_path):
-        self.picture_path = picture_path
+        self.picture_path = Path(picture_path)
         self.image = None
         self._read()
 
--- tests/test_metaPhoto.py
+++ tests/test_metaPhoto.py
@@ -1,4 +1,6 @@
+from pathlib import Path
 from unittest import TestCase
+from unittest.mock import patch
 
 from MetaPhoto.MetaPhoto import MetaPhoto
 
@@ -14,3 +16,12 @@ class TestMetaPhoto(TestCase):
         meta._read_dir()
         meta._read_meta()
         self.assertEqual("2008:11:22 19:29:04", meta.meta_pictures[0]._get_date())
+
+    @patch("MetaPhoto.MetaPhoto.move")
+    def test_move_picture(self, mock_copy):
+        meta = MetaPhoto("../samples/")
+        meta._read_dir()
+        meta._read_meta()
+        meta._move_picture(meta.meta_pictures[0], "/test/folder/")
+        mock_copy.assert_called_once()
+        mock_copy.assert_called_once_with(Path("../samples/astro.jpg"), "/test/folder/2008:11:22 19:29:04_astro.jpg")
