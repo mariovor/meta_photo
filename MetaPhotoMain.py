40264c428f4d1ef5c47fac72fc2d3825176fbce3
Mario Vormstein <github@hquer.eu>
2020-02-22 18:15:50 +0100

    Add a main

---
+++ MetaPhotoMain.py
@@ -0,0 +1,27 @@
+import getopt
+import sys
+
+from MetaPhoto.MetaPhoto import MetaPhoto
+
+
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "i:o:t:", ["input_dir=", "output_dir=", "tag="])
+    except getopt.GetoptError as err:
+        print(err)
+        sys.exit(2)
+    source_directory = None
+    target_directory = None
+    tag = None
+    for option, arg in opts:
+        if option == "-i":
+            source_directory = arg
+        elif option == "-o":
+            target_directory = arg
+        elif option == "-t":
+            tag = arg
+        else:
+            assert False, "unhandled option"
+
+    meta = MetaPhoto(source_directory=source_directory, target_directory=target_directory, tag=tag)
+    meta.copy()
